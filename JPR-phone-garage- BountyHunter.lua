f151b037dbb299bd4dde94fb1952d1d606800477
ilivefreeagain <167541904+ilivefreeagain@users.noreply.github.com>
2024-12-12 02:22:55 -0500

    Add files via upload

---
+++ JPR-phone-garage- BountyHunter.lua
@@ -0,0 +1,51 @@
+ 
+
+JPR-PHONE
+
+
+City contacts 
+
+    {
+        job = "bountyhunter",
+        name = "Bounty",
+        apelido = "Los Santos",
+        number = "415",
+    },
+
+
+charging location
+
+{coords = vector3(475.26, -919.23, 26.41), distance= 5, name = "bountyhunter" },
+
+
+JPR-Garage
+
+{
+        name = "Bottom_Bails",
+        model = "s_m_m_ciasec_01", --- model
+        coords = vector3(490.79, -919.43, 25.32),         -- Coords
+        spawn = {
+            {taken = false, coords = vector4(492.38, -916.01, 26.23, 244)},
+        },
+        store = vector3(494.88, -916.10, 26.08),
+        heading = 238, --- heading
+        gender = "male", --- Gender
+        blipOnMap = true,
+        job = "bountyhunter",
+        licensePlate = "Bounty", -- NO SPACES
+        scenario = "WORLD_HUMAN_CLIPBOARD_FACILITY", --- Ped scenarios
+        canInteract = {
+            cars = true, --- carros
+            bikes = true, ---- motas
+            planes = false, --- avioes
+            boats = false, --- barcos
+        },
+        vehicles = {
+            {model = "comet2", grade = 1, turbo = true, engine = 4, suspension = 2, transmission = 2, brakes = 1, vehColor = {0,0,255}},
+            {model = "baller7", grade = 3, turbo = true, engine = 2, suspension = 2, transmission = 2, brakes = 2, vehColor = {0,0,255}},
+        }
+    },
+
+
+
+    This is for Pickle Mods 
